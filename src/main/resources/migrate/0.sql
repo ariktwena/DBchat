4ef7f52fcd93fd865cfec5ac260113325d5a07b4
ariktwena <ariktwena@gmail.com>
2020-09-27 18:41:53 +0200

    updated reset sql

--- src/main/resources/migrate/0.sql
+++ src/main/resources/migrate/0.sql
@@ -1,17 +1,9 @@
-DROP USER IF EXISTS 'testuser'@'localhost';
-DROP DATABASE IF EXISTS chat;
-
-
-CREATE DATABASE  IF NOT EXISTS chat;
-CREATE USER  'testuser'@'localhost';
-grant all privileges on chat.* TO 'testuser'@'localhost';
-
 USE chat;
 
 DROP TABLE IF EXISTS properties;
 CREATE TABLE properties (
-    name VARCHAR(255) PRIMARY KEY,
-    value VARCHAR(255) NOT NULL
+name VARCHAR(255) PRIMARY KEY,
+value VARCHAR(255) NOT NULL
 );
 
 INSERT INTO properties (name, value) VALUES ("version", "0");
