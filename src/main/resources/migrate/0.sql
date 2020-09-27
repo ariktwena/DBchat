c84a32c5f959f92288caf1a51c11b23972fb138a
ariktwena <ariktwena@gmail.com>
2020-09-27 13:26:32 +0200

    update to DB and to new tables

--- src/main/resources/migrate/0.sql
+++ src/main/resources/migrate/0.sql
@@ -2,11 +2,11 @@ DROP USER IF EXISTS 'testuser'@'localhost';
 DROP DATABASE IF EXISTS chat;
 
 
-CREATE DATABASE  IF NOT EXISTS `chat`;
+CREATE DATABASE  IF NOT EXISTS chat;
 CREATE USER  'testuser'@'localhost';
-grant all privileges on chat.* TO 'testuser'@'localhost';
+grant all privileges on chat.* TO 'testuser'@'localhost' WITH GRANT OPTION;
 
-USE `chat`;
+USE chat;
 
 DROP TABLE IF EXISTS properties;
 CREATE TABLE properties (
--- src/main/resources/migrate/resetTo0.sql
+++ src/main/resources/migrate/resetTo0.sql
@@ -2,11 +2,11 @@ DROP USER IF EXISTS 'testuser'@'localhost';
 DROP DATABASE IF EXISTS chat;
 
 
-CREATE DATABASE  IF NOT EXISTS `chat`;
-CREATE USER  'testuser'@'localhost';
+CREATE DATABASE IF NOT EXISTS chat;
+CREATE USER 'testuser'@'localhost';
 grant all privileges on chat.* TO 'testuser'@'localhost';
 
-USE `chat`;
+USE chat;
 
 DROP TABLE IF EXISTS properties;
 CREATE TABLE properties (
